8ff19ffda87a6d90cb0f8176d751fac1fadd1970
anastasianikola <annasti.bar@gmail.com>
2025-09-15 20:54:16 +0700

    Добавьте файлы проекта.

---
+++ Beauty Salon/DatabaseHelper.cs
@@ -0,0 +1,23 @@
+﻿
+using Beauty_Salon.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beauty_Salon
+{
+    public class DatabaseHelper
+    {
+        private static Beauty_Salon_Entities _context;
+
+        public static Beauty_Salon_Entities GetContext()
+        {
+            if (_context == null)
+                _context = new Beauty_Salon_Entities();
+            return _context;
+        }
+    }
+}
+   
---
+++ Beauty Salon/Pages/PageServices.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Beauty_Salon.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PageServices.xaml
+    /// </summary>
+    public partial class PageServices : Page
+    {
+        public PageServices()
+        {
+            InitializeComponent();
+            LoadServices();
+
+        }
+        private void LoadServices()
+        { 
+            var services = DatabaseHelper.GetContext().Services.ToList();
+
+            LViewProduct.ItemsSource = services;
+        }
+
+        private void LViewProduct_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbSorting_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void cmbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
